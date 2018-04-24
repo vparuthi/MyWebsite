eb082a4d48b1e44e6834ad0f2ad65eddb1be10cc
Veraj <veraj.paruthi@gmail.com>
2018-04-24 14:21:50 -0400

    Additional info panel for grid items added

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 $(document).ready(function() {
-    $('#about, #landing-page, #work, #contact').click(function() {
-        var target = $(this).attr('href');
-        console.log(target);
-        $('html, body').animate({
-          scrollTop: $(target).offset().top
-        }, 1500)
-    });
+
+  $(".btn-light").click(function(){
+    var parents = $(this).parents();
+    // $(parents[2]).css("width", "500px");
+    console.log(parents);
+    console.log($(parents[0]).siblings());
+  //   $(parents[2]).flip({
+  //     trigger: 'manual',
+  //     autoSize: true
+  //   });
+  //   $(parents[2]).flip('toggle');
+  $(parents[0]).siblings().fadeIn();
+  $(parents[0]).fadeOut();
+
+  })
+
 });
